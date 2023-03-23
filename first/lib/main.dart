3b2554ca5f2d18787e25927cedf486efc5f5b248
tunameatball <kh9768@gmail.com>
2023-03-23 22:57:25 +0900

    complete first Wallet Page

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:first/widgets/button.dart';
+import 'package:first/widgets/currency_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
-        body: Padding(
+        body: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child:
               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
@@ -46,7 +47,7 @@ class App extends StatelessWidget {
               ],
             ),
             const SizedBox(
-              height: 80,
+              height: 70,
             ),
             Text(
               'Total Balance',
@@ -81,6 +82,56 @@ class App extends StatelessWidget {
                     bgColor: Color(0xFF1F2123),
                     textColor: Colors.white),
               ],
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Wallets',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.w600,
+                    )),
+                Text('View All',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 18,
+                    )),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const CurrencyCard(
+              name: 'Euro',
+              code: 'EUR',
+              amount: '6 428',
+              icon: Icons.euro_rounded,
+              isInverted: false,
+            ),
+            Transform.translate(
+              offset: const Offset(0, -50),
+              child: const CurrencyCard(
+                name: 'Dollar',
+                code: 'USD',
+                amount: '55 628',
+                icon: Icons.attach_money_rounded,
+                isInverted: true,
+              ),
+            ),
+            Transform.translate(
+              offset: const Offset(0, -100),
+              child: const CurrencyCard(
+                name: 'Bitcoin',
+                code: 'BTC',
+                amount: '500',
+                icon: Icons.currency_bitcoin_outlined,
+                isInverted: false,
+              ),
             ),
           ]),
         ),
