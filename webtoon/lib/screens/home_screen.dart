7516a9d67ef342c937fa26bf7ed26247a867d3ff
tunameatball <kh9768@gmail.com>
2023-03-26 19:48:26 +0900

    create ListView

--- webtoon/lib/screens/home_screen.dart
+++ webtoon/lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ import '../models/webtoon_model.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +27,20 @@ class HomeScreen extends StatelessWidget {
       body: FutureBuilder(
         builder: ((context, snapshot) {
           if (snapshot.hasData) {
-            return const Text("There is Data");
+            return ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  var webtoon = snapshot.data![index];
+                  return Text(webtoon.title);
+                },
+                separatorBuilder: (context, index) => const SizedBox(
+                      width: 20,
+                    ));
           } else {
-            return const Text("Loading ....");
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
         }),
         future: webtoons,
